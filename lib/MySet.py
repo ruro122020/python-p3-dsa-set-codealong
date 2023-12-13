80bee3469d29025fa414c0fc1fd6cb95543e80d1
Ruth <ruro122020@gmail.com>
2023-12-12 21:46:12 -0500

    has method in MySet class complete

--- lib/MySet.py
+++ lib/MySet.py
@@ -5,3 +5,8 @@ class MySet:
             self.dictionary[value] = True
         
     
+    def has(self, value):
+        #return self.dictionary[value]
+        #the above code will not work becuase it will return None instead of False
+        #using the 'in' key word will return true of false
+        return value in self.dictionary
